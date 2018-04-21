d63f9344f433b6aaf465bc3409df66c2dbd47360
wangfei <wffeige@126.com>
2018-04-21 13:45:30 +0800

    add var

---
+++ lesson2/wangfei/lianxi/var.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func variables()  {
+	var i int = 10
+	var s string = "hello world"
+	fmt.Println(i)
+	fmt.Println(s)
+}
+
+func main(){
+	//i := 10
+	//s := "hello world"
+	//fmt.Println(i,s)
+	variables()
+}
