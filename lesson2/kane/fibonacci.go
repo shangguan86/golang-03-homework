f228aee1956613121deaaed596ddb1d5292c4f0c
songkane <songkane@vip.qq.com>
2018-04-21 18:18:38 +0800

    增加计算时间

--- lesson2/kane/fibonacci.go
+++ lesson2/kane/fibonacci.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -13,16 +14,24 @@ func main() {
 	if num <= 0 {
 		fmt.Println("Error")
 	}
-
+	start := time.Now()
 	var ways = fib(num)
 	fmt.Println("递归的结果", ways)
+	elapsed := time.Since(start)
+
+	//输出执行时间。
+	fmt.Println("递归时间", elapsed)
 
 	var i int
 	var first int = 1
 	var second int = 2
+	start1 := time.Now()
 	for i = 1; i < num; i++ {
 		first, second = step(first, second)
 	}
+	elapsed1 := time.Since(start1)
+	//输出执行时间。
+	fmt.Println("迭代时间 ", elapsed1)
 	fmt.Println("循环的结果", first)
 }
 func step(first int, second int) (int, int) {
