b0568aa7ffb1f1c9a8733d8afc2045d163d288dc
Detavern <detavern@live.com>
2018-04-21 16:46:47 +0800

    add flag.go

--- lesson2/zitu/flag.go
+++ lesson2/zitu/flag.go
@@ -9,8 +9,9 @@ func main() {
 	var flagNewLine bool
 	filePath := flag.String("f", "default path", "file path")
 	flag.BoolVar(&flagNewLine, "n", false, "new line flag")
-
 	flag.Parse()
+	fmt.Println(flagNewLine)
+
 	// printFile(*filePath)
 	fmt.Println(*filePath)
 	if flagNewLine {
