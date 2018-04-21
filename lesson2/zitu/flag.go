2795c117c4b92afe0d65bf003f65afec709db04c
Detavern <detavern@live.com>
2018-04-21 16:32:52 +0800

    add flag.go

---
+++ lesson2/zitu/flag.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var flagNewLine bool
+	filePath := flag.String("f", "default path", "file path")
+	flag.BoolVar(&flagNewLine, "n", false, "new line flag")
+
+	flag.Parse()
+	// printFile(*filePath)
+	fmt.Println(*filePath)
+	if flagNewLine {
+		fmt.Println()
+	}
+}
