03b8a3b1eb5b48f7aebb04a786a1f14ff8a94c14
wangfei <wffeige@126.com>
2018-04-21 13:55:26 +0800

    add lianxi

--- lesson2/wangfei/lianxi/var.go
+++ lesson2/wangfei/lianxi/var.go
@@ -1,19 +1,26 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func variables()  {
-	var i int = 10
-	var s string = "hello world"
-	fmt.Println(i)
-	fmt.Println(s)
-}
 
 func main(){
-	//i := 10
-	//s := "hello world"
-	//fmt.Println(i,s)
-	variables()
+	var i int = 10
+	var s string = "hello world1"
+	var f float64 = 1.1
+	fmt.Println(i,s,f)
+
+	var i2 = 20
+	var s2 = "hello world2"
+	var f2 = 2.1
+	fmt.Println(i2,s2,f2)
+
+	var (
+		v1 int
+		v2 string
+		v3 float64
+	)
+	v1 = 30
+	v2 = "hello world3"
+	v3 = 3.1
+	fmt.Printf("%v,%v,%v\n",v1,v2,v3)
 }
