faadc80686a34dcc7e6a2a010312bc86b0ec5ad0
xuezhaomeng <xuezhaomeng@MacdeMac-mini.local>
2018-05-05 10:35:51 +0800

    update

--- lesson2/xuezhaomeng/homework/mycat.go
+++ lesson2/xuezhaomeng/homework/mycat.go
@@ -21,6 +21,11 @@ func r_flag() {
 	flag.Parse() //注意, 如果想获取命令行的指定值, 必须要写入该行
 	fmt.Println(*Fn)
 	printFile(*Fn)
+
+	//var filename string
+	//flag.StringVar(&filename, "f", "./cat file_to_path", "读取某个文件的内容")
+	//flag.Parse()
+	//printFile(filename)
 }
 
 func main() {
