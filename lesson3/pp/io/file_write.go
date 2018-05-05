9acf6ba476a3de2b8ff4daa2b6d6b58f6739a4c3
byan <root@iZq1zotf9u5xo9Z.(none)>
2018-05-05 18:08:30 +0800

    file io

---
+++ lesson3/pp/io/file_write.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "os"
+    //"fmt"
+)
+
+func main() {
+    f, _ := os.Create("a.txt")
+    //f, _ := os.OpenFile("a.txt", os.O_CREATE | os.O_APPEND | os.O_RDWR, 0644)
+    f.WriteString("helloworld!\n")
+    f.Seek(2, os.SEEK_SET)
+    f.WriteString("$$$")
+    f.Close()
+}
