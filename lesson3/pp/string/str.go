9cdb3778f4d54099f00ae5cd652f03dff8dc87eb
byan <root@iZq1zotf9u5xo9Z.(none)>
2018-05-05 15:04:56 +0800

    add basic string operation demo

--- lesson3/pp/string/str.go
+++ lesson3/pp/string/str.go
@@ -2,20 +2,32 @@ package main
 
 import (
     "fmt"
+    "strconv"
+    "reflect"
 )
 
 
 func main(){
-    s1 := "hello" + 
-        "world!"
-
-    s2 := `
-        hello 
-        hello 
-        hello 
-        hello 
-    `
+    // 用+号来连接字符串
+    s1 := "hello" + "world!"
+    //s1 := "中国"
     
-    fmt.Println(s1)
+    // 随机读取字符
+    c := s1[0]
+    fmt.Println(string(c))
+
+    // 切片(包含start, 不包含end)
+    // 输出: hel
+    s2 := s1[0:len(s1)]
     fmt.Println(s2)
+
+    i, _ := strconv.Atoi("-42")
+    fmt.Println(i)
+    fmt.Println("type of i: ", reflect.TypeOf(i))
+
+    f, _ := strconv.ParseFloat("3.14", 64)
+    fmt.Println(f)
+    fmt.Println("type of f: ", reflect.TypeOf(f))
+
+    fmt.Println(s1)
 }
