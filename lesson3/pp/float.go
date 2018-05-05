0a0a70a47b4063ef39258a6e3dae81170025c762
byan <root@iZq1zotf9u5xo9Z.(none)>
2018-05-05 13:46:09 +0800

    add float example

---
+++ lesson3/pp/float.go
@@ -0,0 +1,17 @@
+package main
+
+
+import (
+    "fmt"
+)
+
+
+func main() {
+    var f1 = 0.1110
+    var f2 = 0.1111
+
+    var equal = (f1 - f2) < 0.01
+    fmt.Println(equal)
+    
+    fmt.Println(3.0/2)
+}
