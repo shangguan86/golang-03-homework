9e8741bbae80a8fcddd5ea85013d838143a33e13
wusuopubupt <wdxbupt2009@gmail.com>
2018-05-19 06:52:21 +0000

    list example

--- lesson5/pp/list.go
+++ lesson5/pp/list.go
@@ -12,7 +12,25 @@ type Node struct  {
     Next *Node
 }
 
+func reverseList(p *Node) *Node{
+    var pre *Node 
+    for p != nil {
+        // 暂存 p 的下一个节点
+        next := p.Next
+        // 让当前节点p的下一个节点为pre
+        p.Next = pre
+        // 更新 pre为当前节点
+        pre = p
+        // 继续迭代， 变更 p 为其原来的next节点
+        p = next
+    }
+    // 此时，p是nil, 应当返回pre节点
+    return pre
+}
+
 func main() {
+    fmt.Println(x)
+
     node_a := Node{Val: Student{Id: 1, Name:"A"}}
     node_b := Node{Val: Student{Id: 2, Name:"B"}}
     //node_c := Node{Val: Student{Id: 3, Name:"C"}}
@@ -21,10 +39,7 @@ func main() {
     node_a.Next = &node_b
     //fmt.Println(node_a)
     p := &node_a
-    for p != nil {
-        node := *p
-        fmt.Println(node)
-        p = node.Next
-    }
-    
+    printList((p))
+    fmt.Println()
+    printList(reverseList(p))
 }
---
+++ lesson5/pp/print.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+
+var x = 1
+
+func printList(p *Node) {
+    for p != nil {
+        node := *p
+        fmt.Println(node)
+        p = node.Next
+    }
+}
