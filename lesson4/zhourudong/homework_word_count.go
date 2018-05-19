136d740388bc4e2bac90ba9722d37e4782e12d30
zhourudong <3294563258@qq.com>
2018-05-19 09:37:10 +0800

    Update homework_word_count.go

--- lesson4/zhourudong/homework_word_count.go
+++ lesson4/zhourudong/homework_word_count.go
@@ -24,7 +24,7 @@ func count_word_method2(words []string) {
 
 	for _, key := range words {
 		// 判断map中是否有这个key,有则加1,否则初始化此key的值为1
-		if _, ok := count_words[key]; ok {
+		if _, ok := count_words[key]; !ok {
 			count_words[key] = 1
 		} else {
 			count_words[key] = count_words[key] + 1
