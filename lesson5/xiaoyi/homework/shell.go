6cd1348d00bc1b2e51cb244277c3b15b542675b6
yangsheng <yang972711021@gamil.com>
2018-05-25 18:19:01 +0800

    shell作业提交

--- lesson5/xiaoyi/homework/shell.go
+++ lesson5/xiaoyi/homework/shell.go
@@ -1,2 +1,40 @@
 package main
 
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"strings"
+	"os/exec"
+)
+
+func main() {
+	host,_ := os.Hostname() //获取主机名
+	prompt := fmt.Sprintf("[xiaoyi@%s]",host) //将参数列表"host"填写到格式化字符串"[xiaoyi@%s]"中的占位符;
+	scanne := bufio.NewScanner(os.Stdin)
+
+	for  {
+		fmt.Print(prompt)
+		scanne.Scan()
+		line := scanne.Text()
+
+		if len(line) == 0 {
+			continue
+		}
+
+		args := strings.Fields(line)
+		cmd := exec.Command(args[0],args[1:]...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if line == "exit" {
+			break
+		}
+	}
+}
