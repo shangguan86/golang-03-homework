11c6c6a8341a66f02de9c95b26e936e526b120d0
tukoyi <chaseydd@qq.com>
2018-05-29 17:28:57 +0800

    feat:add sortByLength

--- lesson6/yuandongdong/homework/sortByLength.go
+++ lesson6/yuandongdong/homework/sortByLength.go
@@ -7,9 +7,17 @@ import (
 
 type StringSlice []string
 
-func (p StringSlice) Len() int           { return len(p) }
-func (p StringSlice) Less(i, j int) bool { return len(p[i]) < len(p[j]) }
-func (p StringSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p StringSlice) Len() int {
+	return len(p)
+}
+
+func (p StringSlice) Less(i, j int) bool {
+	return len(p[i]) < len(p[j])
+}
+
+func (p StringSlice) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
 
 func main() {
 	s := []string{
